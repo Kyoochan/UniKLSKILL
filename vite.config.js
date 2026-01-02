88efccd78ad0041af7d862cc77ef2279a67c7978
Kyoochan <kyosai1805@gmail.com>
2026-01-02 18:56:49 +0800

    manifest fix v2.2

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: false,
+            refresh: true,
         }),
+        tailwindcss(),
     ],
-    build: {
-        manifest: true,          // generate manifest.json
-        outDir: 'public/build',  // must be a folder
-        emptyOutDir: true,       // clean old builds
+    server: {
+        cors: true,
     },
 });
